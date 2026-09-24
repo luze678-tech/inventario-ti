eecc805a5a307cfb1a74edd8d16549469a69373d
Marisol Toscano Yepez <luce678@gmail.com>
2026-09-24 12:21:51 -0600

    Guardar equipos en archivo de texto

--- app.py
+++ app.py
@@ -1,4 +1,33 @@
-print("Hola Git")
-print("Inventario TI")
-print("Version 2")
-print("Version en desarrollo")
+equipos = []
+
+while True:
+    print("\n--- INVENTARIO TI ---")
+    print("1. Agregar equipo")
+    print("2. Listar equipos")
+    print("3. Salir")
+
+    opcion = input("Selecciona una opción: ")
+
+    if opcion == "1":
+        nombre = input("Nombre del equipo: ")
+        serie = input("Número de serie: ")
+
+        with open("equipos.txt", "a") as archivo:
+            archivo.write(f"{nombre} - {serie}\n")
+
+        equipos.append({
+            "nombre": nombre,
+            "serie": serie
+        })
+
+        print("Equipo agregado.")
+
+    elif opcion == "2":
+        with open("equipos.txt", "r") as archivo:
+            print(archivo.read())
+
+    elif opcion == "3":
+        break
+
+    else:
+        print("Opción inválida")
